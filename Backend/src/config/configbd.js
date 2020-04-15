6b9407eb68b0fcf4424175129826747dbce05a91
ypernillo <yimmiss305@gmail.com>
2020-04-15 00:08:32 -0600

    Configuracion de la conexion con la Base de datos.

---
+++ Backend/src/config/configbd.js
@@ -0,0 +1,17 @@
+const oracledb = require('oracledb');
+
+cns = {
+    user: "BD",
+    password: "3887",
+    connectString: "172.17.0.2/ORCLCDB"
+}
+
+
+async function Open(sql, binds, autoCommit) {
+    let cnn = await oracledb.getConnection(cns);
+    let result = await oracledb.execute(sql, binds, { autoCommit });
+    cnn.release();
+    return result;
+}
+
+exports.Open = Open;
