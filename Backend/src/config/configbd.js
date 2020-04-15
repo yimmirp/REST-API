177b57822d397f8a81329fa6536fa2b79af6defb
ypernillo <yimmiss305@gmail.com>
2020-04-15 00:17:06 -0600

    Declaracion de las Librerias.

--- Backend/src/config/configbd.js
+++ Backend/src/config/configbd.js
@@ -9,7 +9,7 @@ cns = {
 
 async function Open(sql, binds, autoCommit) {
     let cnn = await oracledb.getConnection(cns);
-    let result = await oracledb.execute(sql, binds, { autoCommit });
+    let result = await cnn.execute(sql, binds, { autoCommit });
     cnn.release();
     return result;
 }
--- Backend/src/routes/person-rotes.js
+++ Backend/src/routes/person-rotes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 const router = Router();
+const BD = require('../config/configbd');
 
+router.get('/', async (req, res) => {
+    sql = "select * from person where state=1";
 
-router.get('/', (req, res) => {
-
+    let result = await BD.Open(sql, [], false);
+    console.log(result.rows);
     res.json('TODO OKEII');
 })
 
